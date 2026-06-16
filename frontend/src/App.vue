63166945fc0cba5d28de3db902723778661b0513
@sklyarvlad <sklyarvladislavtl@gmail.com>
2026-06-16 16:36:58 +0300

    create start frontend + backend + db and in docker-compose

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page">
+    <div class="card">
+      <h1>Hi, its my first site</h1>
+      <h2>I'm try to input this frontend in docker</h2>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref<number>(0)
+</script>
+
+<style>
+.page {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: var(--background);
+}
+.card {
+  color: #03451e;
+}
+</style>
